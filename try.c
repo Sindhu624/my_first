d5802d5284a9439de992e9ec73f9580e950a2d48
Sindhu624 <Sindhu624>
2019-09-10 17:54:31 +0530

    My life first git commit

---
+++ try.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+ float f=0.1;
+ 
+ if(f==0.1f)
+ 
+ 	printf("true");
+ else
+	printf("false");
+}
---
+++ try1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+int x;
+printf("enter no:\n");
+scanf("%d",&x);
+if(x%2==1)
+{
+	printf("odd");
+}
+else
+{printf("even");
+}
+}
